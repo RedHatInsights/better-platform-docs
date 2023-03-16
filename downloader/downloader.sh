4282b9cf8ba9cbae7fcb611d735925d04212565a
Karel Hala <khala@redhat.com>
2023-03-16 12:49:51 +0100

    Add code examples

--- downloader/downloader.sh
+++ downloader/downloader.sh
@@ -4,14 +4,14 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 INPUT=`tail -n +1 $SCRIPTPATH/config.csv`
 OLDIFS=$IFS
 IFS=','
-while read owner repo branch path folder public
+while read owner repo branch path folder
 do
   curl -o /tmp/${repo}.zip https://github.com/${owner}/${repo}/archive/refs/heads/${branch}.zip -O -J -L
   unzip /tmp/${repo}.zip -d /tmp
   rm -f /tmp/${repo}.zip
   FOLDER=`echo $folder | tr ' ' '-' | tr '[:upper:]' '[:lower:]'`
-  if [ "$FOLDER" = "true" ]; then
-    rsync -a -v /tmp/${repo}-${branch}/${path} $SCRIPTPATH/../public/$FOLDER
+  if [ "$FOLDER" = "public" ]; then
+    rsync -a -v /tmp/${repo}-${branch}/${path} $SCRIPTPATH/../public
   else
     rsync -a -v /tmp/${repo}-${branch}/${path} $SCRIPTPATH/../pages/$FOLDER
   fi
