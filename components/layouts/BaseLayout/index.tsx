b53eb77fabdaf1f2c19632eb6dded446829e3b5e
epwinchell <1287144+epwinchell@users.noreply.github.com>
2023-06-28 10:55:39 -0400

    landing page updates (#17)
    * landing page updates
    
    * updates
    
    * another update
    
    * rebase fixes
    
    ---------
    
    Co-authored-by: Karel Hala <khala@redhat.com>

--- components/layouts/BaseLayout/index.tsx
+++ components/layouts/BaseLayout/index.tsx
@@ -1,7 +1,17 @@
 import {
+  Icon,
+  Hint,
+  HintTitle,
+  Gallery,
+  HintBody,
+  Text,
+  TextContent,
   Page,
   PageHeader,
+  PageSection,
+  PageSectionVariants,
   PageSidebar,
+  SearchInput,
   Split,
   SplitItem,
   Stack,
@@ -12,6 +22,8 @@ import classnames from "clsx";
 import { PropsWithChildren, useState } from "react";
 import { createUseStyles } from "react-jss";
 import Image from "next/image";
+import FilterIcon from "@patternfly/react-icons/dist/js/icons/filter-icon";
+
 import Navigation from "../../Navigation";
 import useNavSchema from "../../Navigation/useNavSchema";
 import TableOfContents from "../../table-of-contents";
@@ -23,37 +35,33 @@ const useStyles = createUseStyles({
   logo: {
     width: 100,
   },
-  platExGuy: {
-    maxHeight: "100%",
+  banner: {
+    backgroundImage:
+      "url(https://console.redhat.com/apps/frontend-assets/platform-doc/ExternalDocsSiteBackground.svg)",
   },
-  content: {
-    marginLeft: "auto",
-    marginRight: "auto",
-    width: "calc(100% - 16px * 2)",
-    display: "flex",
-    flexDirection: "column",
+  banner_content: {
+    maxWidth: "1000px",
+    margin: "auto",
   },
-  tableOfContents: {
+  icon: {
+    position: "absolute",
+    marginLeft: "var(--pf-global--spacer--sm)",
+    marginTop: "var(--pf-global--spacer--sm)",
+    zIndex: "2",
+  },
+  filter: {
+    width: "360px",
+    "& svg": {
+      display: "none",
+    },
+  },
+  search_wrapper: {
+    position: "relative",
+    width: "360px",
+    margin: "auto",
+  },
+  tableOfContent: {
     display: "none",
-  },
-  "@media (min-width: 1200px)": {
-    content: {
-      width: 900,
-    },
-    tableOfContents: {
-      display: "block",
-    },
-  },
-  link: {
-    textDecoration: "none",
-    color: "var(--pf-global--BackgroundColor--100)",
-  },
-  footer: {
-    height: "auto",
-    background: "var(--pf-global--BackgroundColor--dark-100)",
-  },
-  contentWrapper: {
-    flex: 1,
   },
 });
 
@@ -92,18 +100,45 @@ const BaseLayout: React.FC<PropsWithChildren> = ({ children }) => {
       className={classes.page}
       header={Header}
     >
-      <Split style={{ minHeight: "76.9vh" }} hasGutter>
-        <SplitItem isFilled>
-          <div className={classnames("pf-u-p-md", classes.content)}>
-            <Stack hasGutter>
-              <StackItem id="docs-content">{children}</StackItem>
-            </Stack>
-          </div>
-        </SplitItem>
-        <SplitItem className={classes.tableOfContents}>
+      <PageSection isFilled={false} className={classes.banner} hasShadowBottom>
+        <div className={classes.banner_content}>
+          <TextContent className="pf-u-text-align-center pf-u-mb-md">
+            <Text component="h1" className="pf-u-pt-xl">
+              Welcome to the Platform Experience internal docs site!
+            </Text>
+            <Text component="p" className="pf-u-pt-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            </Text>
+          </TextContent>
+          <Hint className="pf-u-mb-2xl">
+            <HintTitle>News and announcements</HintTitle>
+            <HintBody>
+              React router v6 upgrade HMR available in development
+            </HintBody>
+          </Hint>
+        </div>
+      </PageSection>
+      <PageSection id="docs-content">
+        <TextContent className="pf-u-pt-md pf-u-text-align-center">
+          <Text component="h1">All PlatEx documentation</Text>
+        </TextContent>
+        <div className={classes.search_wrapper}>
+          <Icon className={classes.icon}>
+            <FilterIcon />
+          </Icon>
+          <SearchInput
+            className={classnames(classes.filter, "pf-u-mt-md pf-u-mb-xl")}
+            data-ouia-component-id="app-filter-search"
+            placeholder="Find documentation ..."
+          />
+        </div>
+        {children}
+        <div className={classes.tableOfContent}>
           <TableOfContents />
-        </SplitItem>
-      </Split>
+        </div>
+      </PageSection>
     </Page>
   );
 };
