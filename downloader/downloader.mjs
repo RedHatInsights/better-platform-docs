a563d3989ee39cd0fb022c7cb69c0bbf6965d61c
Karel Hala <khala@redhat.com>
2023-07-04 12:32:05 +0200

    Fix proxy pass for root URL (#30)

--- downloader/downloader.mjs
+++ downloader/downloader.mjs
@@ -5,32 +5,51 @@ import fs from "fs";
 import { exec } from "child_process";
 
 data.forEach(({ owner, repository, branch, path, title }) => {
+  console.log(`Repository: ${owner}/${repository}`);
+  console.log(`Using branch ${branch}`);
   const onGenerate = () => {
     if (title === "public") {
-      exec(`cp -r /tmp/${repository}-${branch}/${path} ./public`);
+      console.log(
+        "Extracting to public folder: ",
+        `./tmp/${repository}-${branch}/${path}`
+      );
+      exec(`cp -r ./tmp/${repository}-${branch}/${path} ./public`);
     } else if (title === "examples") {
-      exec(`rsync -a -v /tmp/${repository}-${branch}/${path} ./components`);
+      console.log(
+        "Extracting to components folder: ",
+        `./tmp/${repository}-${branch}/${path}`
+      );
+      exec(`rsync -a -v ./tmp/${repository}-${branch}/${path} ./components`);
     } else {
+      console.log(
+        `Extracting to ./pages/${title.replaceAll(" ", "-").toLowerCase()}`,
+        `./tmp/${repository}-${branch}/${path}`
+      );
       exec(
-        `rsync -a -v /tmp/${repository}-${branch}/${path} ./pages/${title
+        `rsync -a -v ./tmp/${repository}-${branch}/${path} ./pages/${title
           .replaceAll(" ", "-")
           .toLowerCase()}`
       );
     }
   };
-  if (fs.existsSync(`/tmp/${repository}-${branch}`)) {
+  if (fs.existsSync(`./tmp/${repository}-${branch}`)) {
     console.log("folder exists!");
     onGenerate();
   } else {
-    const file = fs.createWriteStream(`/tmp/${repository}.zip`);
+    console.log(
+      "Downloading files",
+      `https://github.com/${owner}/${repository}/archive/refs/heads/${branch}.zip`
+    );
+    const file = fs.createWriteStream(`./tmp/${repository}.zip`);
     follow.https.get(
       `https://github.com/${owner}/${repository}/archive/refs/heads/${branch}.zip`,
       function (response) {
+        console.log("Status: ", response.statusCode);
         response.pipe(file);
         file.on("finish", () => {
           file.close();
-          const zip = new StreamZip.async({ file: `/tmp/${repository}.zip` });
-          zip.extract(null, "/tmp");
+          const zip = new StreamZip.async({ file: `./tmp/${repository}.zip` });
+          zip.extract(null, "./tmp");
           onGenerate();
         });
       }
