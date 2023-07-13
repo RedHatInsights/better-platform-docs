ab01d2acee68be27ec9d76608ff779e268b9120f
Martin Maroši <marvusm.mmi@gmail.com>
2023-07-13 14:51:14 +0200

    Remove custom codeblock adoc writer.

--- downloader/adoc-writer.js
+++ downloader/adoc-writer.js
@@ -2,24 +2,13 @@
 
 const pandoc = require("pandoc-filter-promisified");
 const fs = require("fs");
-const { CodeBlock, Image } = pandoc;
+const { Image } = pandoc;
 
 const outputLogger = new console.Console(fs.createWriteStream("./output.log"));
 
 async function action(elt) {
   if (process.env.DEBUG === "true") {
     outputLogger.log(elt);
-  }
-  if (elt.t === `CodeBlock`) {
-    // console.warn(JSON.stringify(elt, null, 4));
-    const [headers, content] = elt.c;
-    const lang =
-      elt?.a?.language ||
-      headers
-        .flat()
-        .find((item) => typeof item === "string" && item.length > 0);
-    const newContent = "```" + lang || "" + "\n" + content + "\n" + "```";
-    return CodeBlock(headers, newContent);
   }
 
   if (elt.t === "Image") {
